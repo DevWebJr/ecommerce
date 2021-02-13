4c4bc8518968694a0630c6c77be6f831748bf723
DevWebJr <devwebjr@gmail.com>
2021-02-13 01:35:47 +0100

    update css

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,18 +3,37 @@
 namespace App\Controller;
 
 use App\Classes\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
     /**
      * @Route("/panier", name="cart")
      */
     public function index(Cart $cart)
     {
-        dd($cart->get());
-        return $this->render('cart/index.html.twig');
+        $cartComplete = [];
+        
+        foreach($cart->get() as $id => $quantity)
+        {
+            $cartComplete[] = [
+                'product' => $this->entityManager->getRepository(Product::class)->findById($id),
+                'quantity' => $quantity
+            ];
+        }
+
+        return $this->render('cart/index.html.twig', [
+            'cart' => $cartComplete
+        ]);
     }
 
     /**
