5568d1ea551bc431f367003bc794b23b731136a5
DevWebJr <devwebjr@gmail.com>
2021-02-15 23:56:44 +0100

    add some entities:Carrier, Order, and OrderDetails. Manage Carrier by using EasyAdmin view on site.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Carrier;
 use App\Entity\Category;
 use App\Entity\Product;
 use App\Entity\User;
@@ -33,6 +34,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class);
         yield MenuItem::linkToCrud('Catégories', 'fas fa-list', Category::class);
         yield MenuItem::linkToCrud('Produits', 'fas fa-cubes', Product::class);
+        yield MenuItem::linkToCrud('Transporteurs', 'fas fa-shipping-fast', Carrier::class);
     }
 }
 
