63a00fe94ff4e1f84c502f9f9ad361f06d5543c9
Tito <50543732+DevWebJr@users.noreply.github.com>
2022-01-04 16:45:49 +0100

    customize and add some stylesheet

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PagesController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('pages/index.html.twig');
+    }
+}
