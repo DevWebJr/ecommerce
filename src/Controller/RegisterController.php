3a0702ee26843304dbd7cbf380022b868820ebe2
Tito <50543732+DevWebJr@users.noreply.github.com>
2022-01-05 16:13:16 +0100

    generate urls about user

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
@@ -21,7 +22,7 @@ class RegisterController extends AbstractController
     /**
      * @Route("/inscription", name="register")
      */
-    public function index(Request $request): Response
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
@@ -31,6 +32,10 @@ class RegisterController extends AbstractController
         if($form->isSubmitted() && $form->isValid())
         {
             $user = $form->getData();
+
+            $password = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
             $this->entityManager->persist($user);
             $this->entityManager->flush();
         }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
@@ -19,18 +20,30 @@ class RegisterType extends AbstractType
         $builder
             ->add('lastname', TextType::class, [
                 'label' => false,
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' =>33
+                ]),
                 'attr' => [
                     'placeholder' => 'Nom'
                 ]
             ])
             ->add( 'firstname', TextType::class, [
                 'label' => false,
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' =>33
+                ]),
                 'attr' => [
                     'placeholder' => 'Prénom'
                 ]
             ])
             ->add('email', EmailType::class, [
                 'label' => false,
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' =>33
+                ]),
                 'attr' => [
                     'placeholder' => 'Email'
                 ]
